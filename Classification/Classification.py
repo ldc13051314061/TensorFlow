cab938c02e3300dbce9ac7e5d9432b9baff6402c
Decheng96 <13051314061@163.com>
2018-03-15 11:29:09 +0800

    Add files via upload

---
+++ Classification/Classification.py
@@ -0,0 +1,67 @@
+#classification
+#2018.03.15
+import tensorflow as tf
+import numpy as np
+
+import matplotlib as mpl
+mpl.use("Agg")
+import matplotlib.pyplot as plt
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets('MNIST_data',one_hot=True)
+
+def add_layer(inputs,in_size,out_size,activation_function = None):
+    Weights = tf.Variable(tf.random_normal([in_size,out_size]))
+    biases = tf.Variable(tf.zeros([1,out_size]) + 0.1)
+    Wx_plus_b = tf.matmul(inputs,Weights) + biases
+    if activation_function is None:
+        outputs = Wx_plus_b
+    else:
+        outputs = activation_function(Wx_plus_b)
+    return outputs
+
+def compute_accuracy(v_xs,v_ys):
+    global prediction
+    y_pre = sess.run(prediction,feed_dict= {xs:v_xs})
+    correct_prediction = tf.equal(tf.arg_max(y_pre,1),tf.arg_max(v_ys,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+    result = sess.run(accuracy,feed_dict= {xs:v_xs,ys:v_ys})
+    return result
+
+xs = tf.placeholder(tf.float32,[None,28*28])
+ys = tf.placeholder(tf.float32,[None,10])
+
+prediction = add_layer(xs,28*28,10,activation_function=tf.nn.softmax)
+
+#loss
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction),reduction_indices=[1]))
+
+train_step = tf.train.GradientDescentOptimizer(0.1).minimize(cross_entropy)
+
+sess = tf.Session()
+
+init = tf.global_variables_initializer()
+sess.run(init)
+
+fid = open('accuracy.txt','w')
+fid.truncate()#清空文件
+
+for i in range(3001):
+    batch_xs,batch_ys = mnist.train.next_batch(100)
+    sess.run(train_step,feed_dict={xs:batch_xs,ys:batch_ys})
+    if i % 50 ==0:
+        acc = compute_accuracy(mnist.test.images,mnist.test.labels)
+        print(i,'正确率',acc)
+        fid.write(str(i) + '     ' + 'acc='  + str(acc) + '\n')
+
+
+fid.close()
+
+
+
+
+
+
+
+
